cf0ba93aa1619c896436cfa52a6b8a89b61f1061
LMaverick <91562063+LMaverick@users.noreply.github.com>
2023-05-31 21:48:30 -0300

    up

---
+++ js.js
@@ -0,0 +1,15 @@
+function calculateDamage() {
+    const player1Attack = parseInt(document.getElementById('player1-attack').value);
+    const player1Resistance = parseInt(document.getElementById('player1-resistance').value);
+    const player1MagicAttack = parseInt(document.getElementById('player1-magic-attack').value);
+    const player1Mana = parseInt(document.getElementById('player1-mana').value);
+  
+    const player2Resistance = parseInt(document.getElementById('player2-resistance').value);
+  
+    const physicalDamage = player1Attack - player2Resistance / 2;
+    const magicDamage = (player1MagicAttack + player1Mana) - player2Resistance / 2;
+  
+    const result = document.getElementById('result');
+    result.innerHTML = `Dano Físico: ${physicalDamage}<br>Dano Mágico: ${magicDamage}`;
+  }
+  
